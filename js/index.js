f108d2d4dc0dba7ab44146f149a49f3efa20f14c
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-04-17 09:57:00 +0200

    2

--- js/index.js
+++ js/index.js
@@ -19,17 +19,16 @@
 // Модальне вікно rent (thank.html)
 
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
-
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-
+    const openBtns  = document.querySelectorAll("[data-modal-open]");
+    const closeBtns = document.querySelectorAll("[data-modal-close]");
+    const modal     = document.querySelector("[data-modal]");
+  
+    openBtns.forEach(btn  => btn.addEventListener("click", toggleModal));
+    closeBtns.forEach(btn => btn.addEventListener("click", toggleModal));
+  
     function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-        document.body.classList.toggle("no-scroll");
+      modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
     }
-})();
+  })();
+  
